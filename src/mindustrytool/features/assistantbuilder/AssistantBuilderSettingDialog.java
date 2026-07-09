65ea73b44dade85d4fb6de2fede521153935f4b5
Ainss.Code <ainstheundeads@gmail.com>
2026-07-09 19:52:13 +0800

    Create AssistantBuilderSettingDialog.java

---
+++ src/mindustrytool/features/assistantbuilder/AssistantBuilderSettingDialog.java
@@ -0,0 +1,45 @@
+package mindustrytool.features.assistantbuilder;
+
+import arc.Core;
+import mindustry.gen.Tex;
+import mindustry.ui.dialogs.BaseDialog;
+
+public class AssistantBuilderSettingDialog extends BaseDialog {
+    private final AssistantBuilderFeature feature;
+
+    public AssistantBuilderSettingDialog(AssistantBuilderFeature feature) {
+        super("@assistantbuilder.settings.title");
+        this.feature = feature;
+        addCloseButton();
+        shown(this::rebuild);
+    }
+
+    private void rebuild() {
+        cont.clear();
+
+        if (!feature.isEnabled()) {
+            cont.add("@assistantbuilder.settings.disabled").row();
+            return;
+        }
+
+        cont.pane(t -> {
+            t.top();
+            t.add("@assistantbuilder.settings.style-label").left().padBottom(10).row();
+
+            for (ArchitecturalStyle style : ArchitecturalStyle.values()) {
+                boolean selected = feature.getStyle() == style;
+
+                t.table(selected ? Tex.buttonDown : Tex.button, c -> {
+                    c.left();
+                    c.check("", selected, checked -> {
+                        if (checked) {
+                            feature.setStyle(style);
+                            rebuild();
+                        }
+                    }).padRight(10);
+                    c.label(() -> Core.bundle.get(style.nameKey)).left();
+                }).growX().pad(5).row();
+            }
+        }).grow();
+    }
+}
